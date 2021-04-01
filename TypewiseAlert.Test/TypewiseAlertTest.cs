3d7eadb60c093e3e71e93d9889edc767b7c7f615
ucancallmeomi <74286342+ucancallmeomi@users.noreply.github.com>
2021-04-02 01:50:22 +0530

    Resolved build issues

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -12,31 +12,5 @@ namespace TypewiseAlert.Test
             Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
               BMS_Constants.BreachType.TOO_LOW);
         }
-
-        [Fact]
-        public void InfersBreachNormalLimits()
-        {
-            Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-              BMS_Constants.BreachType.NORMAL);
-        }
-
-        [Fact]
-        public void CheckAlertTypeEmail()
-        {
-            BatteryCharacter batteryCharacter = new BatteryCharacter();
-            batteryCharacter.coolingType = CoolingType.MED_ACTIVE_COOLING;
-            batteryCharacter.brand = "Exide";
-            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -1));
-
-        }
-        [Fact]
-        public void CheckAlertTypeController()
-        {
-            BatteryCharacter batteryCharacter = new BatteryCharacter();
-            batteryCharacter.coolingType = CoolingType.PASSIVE_COOLING;
-            batteryCharacter.brand = "Luminuous";
-            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 25));
-        }
-
     }
 }
